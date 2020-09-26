b467ef6ce4709c2c7ce4fac99f1b8f64389d9bb0
zacanger <zac@zacanger.com>
2020-09-26 10:37:27 -0600

    chore: ts

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-* 0.4.0 - 30 September, 2017 - Echo versions after install
-* 0.3.0 - 22 July, 2017 - Now installs latest `npx` as well
-* 0.2.2 - 14 July, 2017 - Fix description in package.json
-* 0.2.1 - 14 July, 2017 - Fix misplaced `trim`
-* 0.2.0 - 14 July, 2017 - 'Actually fix the thing' thanks to @zkat!
-* 0.1.0 - 10 July, 2017 - This does something now
--- index.js
+++ index.js
@@ -1,28 +1,3 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-var, no-process-exit */
-
-var cp = require('child_process')
-var path = require('path')
-var arg = process.argv[2]
-
-if (arg) {
-  if (arg === '-h' || arg === '--help') {
-    console.log('dist-upgrade usage:')
-    console.log('dist-upgrade    # install latest global Node, npm, and npx')
-    console.log('dist-upgrade -h # show this help')
-    process.exit(0)
-  }
-}
-
-var npxBin = path.join(__dirname, 'node_modules', '.bin', 'npx')
-
-cp.spawn(
-  npxBin +
-    ' n latest && npm i -g npm@next && npm i -g npx && echo "node `node -v`; npm `npm -v`; npx `npx -v`"',
-  [],
-  {
-    stdio: 'inherit',
-    shell: true,
-  }
-)
+require('./lib')
