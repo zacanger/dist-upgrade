2e07bf36b75a57be3378e7c85fdc781377b4f2c9
zacanger <zac@zacanger.com>
2022-07-09 17:50:02 -0600

    chore: updates

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
 
-require('./lib')
+const { spawn } = require('child_process')
+const { join } = require('path')
+
+const arg = process.argv[2]
+
+if (arg) {
+  if (arg === '-h' || arg === '--help') {
+    console.log('dist-upgrade usage:')
+    console.log('dist-upgrade    # install latest global Node, npm, and npx')
+    console.log('dist-upgrade -h # show this help')
+    process.exit(0)
+  }
+}
+
+const npxBin = join(__dirname, 'node_modules', '.bin', 'npx')
+
+spawn(
+  npxBin +
+    ' n latest && npm i -g npm@next && npm i -g npx && echo "node `node -v`; npm `npm -v`; npx `npx -v`"',
+  [],
+  {
+    stdio: 'inherit',
+    shell: true
+  }
+)
